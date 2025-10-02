91aa09bbfcf08755272032149295cc7051331a9f
Joshua Wornyuie Ketor <joshuaketor@proton.me>
2025-10-02 08:39:30 +0000

    Add getHumanChoice() function

--- javascript.js
+++ javascript.js
@@ -3,5 +3,12 @@ console.log("Hello World");
 const options = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
-    return options[Math.floor(Math.random() * 10) % 3];
+  return options[Math.floor(Math.random() * 10) % 3];
+}
+
+function getHumanChoice() {
+  const userChoice = prompt(
+    "Input your choice from the following:\n1.Rock\n2.Paper\n3.Scissors"
+  );
+  return userChoice;
 }
