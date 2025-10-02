d82ce4c1575445a8271a3747b1c4397f736beb23
Joshua Wornyuie Ketor <joshuaketor@proton.me>
2025-10-02 09:09:02 +0000

    Add logic to play a single round

--- javascript.js
+++ javascript.js
@@ -14,3 +14,50 @@ function getHumanChoice() {
   );
   return userChoice;
 }
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    if (humanChoice == "rock") {
+        switch(computerChoice) {
+            case "rock":
+                console.log("It's a tie. You both selected Rock.");
+                break;
+            case "paper":
+                console.log("You lose. Paper beats Rock.");
+                computerScore++;
+                break;
+            case "scissors":
+                console.log("You win! Rock beats scissors");
+                humanScore++;
+                break;
+        }
+    } else if (humanChoice == "paper") {
+        switch(computerChoice) {
+            case "rock":
+                console.log("You win! Paper beats Rock.");
+                humanScore++;
+                break;
+            case "paper":
+                console.log("It's a tie. You both selected Paper.");
+                break;
+            case "scissors":
+                console.log("You Lose! Scissor beats Paper");
+                computerScore++;
+                break;
+        }
+    } else if (humanChoice == "scissor") {
+        switch(computerChoice) {
+            case "rock":
+                console.log("You Lose! Rock beats Scissor.");
+                computerScore++;
+                break;
+            case "paper":
+                console.log("You Win!. Scissor beats Paper.");
+                humanScore++;
+                break;
+            case "scissors":
+                console.log("It's a tie. You both selected Scissor");
+                break;
+        }
+    }
+}
